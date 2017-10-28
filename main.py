946c22809cbc2cb94a5e57ea70e2f4cb615efec8
Matt Graham <matthew.graham@gmail.com>
2017-10-28 15:34:29 -0400

    10/28 changes. Pulled from only selenium and exports to csv file

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-# Importing important modules required in making of News Machine
+# Importing required modules
 import time
-from bs4 import BeautifulSoup
 import requests
+import csv
+import pandas as pd
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+browser = webdriver.Firefox() 
 
 #This function will pull nba stats
 def stats():
-    page = driver.get('http://stats.nba.com/game/0021700001/')
-    html = page.page_source
-    soup = BeautifulSoup(html)
-    box = soup.findall("li")
-    print(box)
-    # for x in range(len(stats.find_all("a"))):
-    # 	news = (topstories[0].find_all("a")[x]).get_text().strip()
-    # 	time.sleep(12)
-
+    browser.get('http://stats.nba.com/game/0021700001/')
+    stats = browser.find_element_by_class_name('nba-stat-table')
+    ofile = open('ttest.csv', "w")
+    csvwriter = csv.writer(ofile)
+    csvwriter.writerow(stats.text)
+    ofile.close()
+    #df = pd.DataFrame(eval(stats.text))
+    #print(df.head()) 
+    
 stats()
