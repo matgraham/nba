34193bebe77acce154458bfa28df7d90096fd316
Matt Graham <matthew.graham@gmail.com>
2017-12-09 13:55:08 -0500

    12/9 commit: added loop for pulling multiple urls

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox() 
 
 #This function will pull up the correct webpage and feed that browser object to the stats and games functions. 
-def webpull():
-    browser.get('http://stats.nba.com/game/0021700001/')
+def webpull(url):
+    browser.get(url)
     stats(browser)
     games(browser)
 
@@ -26,7 +26,7 @@ def webpull():
 #This function pulls the player stats and adds them to the stats.csv file
 def stats(browser):
     stats = browser.find_element_by_class_name('game-view')
-    ofile = open('stats.csv', "w", newline='\n')
+    ofile = open('stats.csv', "a", newline='\n')
     writer = csv.writer(ofile)
     statsText = stats.text
     statsText = statsText.split('\n')
@@ -40,7 +40,7 @@ def stats(browser):
 #This function pulls game data and adds them to the games.csv file
 def games(browser):
     game = browser.find_element_by_class_name('stats-game-summary')
-    ofile = open('games.csv',"w")
+    ofile = open('games.csv',"a")
     writer = csv.writer(ofile)
     gameText = game.text
     gameText = gameText.split('\n')
@@ -48,7 +48,8 @@ def games(browser):
         writer.writerow(i)
     ofile.close()
 
-webpull()
+for i in range(2):
+    webpull('http://stats.nba.com/game/002170000{}/'.format(i + 1))
 browser.quit()
 
 #TODO: Create an iterable that will go through the NBA games.
